c9bb80e16c3166c552e3b0b4d29a0462c3254739
Alessandro Bruni <brun@itu.dk>
2023-12-05 09:41:54 +0100

    wip: move to infotheo 0.6.0

--- robustmean.v
+++ robustmean.v
@@ -1,24 +1,26 @@
-From mathcomp Require Import all_ssreflect ssralg fingroup perm finalg matrix.
-From mathcomp Require Import all_algebra. (* for GRing.Theory *)
-From mathcomp Require boolp classical_sets. (* classical_sets for pointedType *)
-From mathcomp Require Import Rstruct topology. (* topology for fct_ringType *)
-Require Import Reals Lra ROrderedType.
-From infotheo Require Import ssrR Reals_ext logb ssr_ext ssralg_ext bigop_ext.
-From infotheo Require Import Rbigop proba fdist.
+From mathcomp Require Import all_ssreflect ssralg ssrnum matrix.
+From mathcomp Require boolp.
+From mathcomp Require Import Rstruct reals.
+Require Import Reals Lra.
+From infotheo Require Import ssrR Reals_ext realType_ext logb ssr_ext ssralg_ext.
+From infotheo Require Import bigop_ext fdist proba.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Local Open Scope proba_scope.
 Local Open Scope R_scope.
+Local Open Scope reals_ext_scope.
+Local Open Scope fdist_scope.
+Local Open Scope proba_scope.
 
-Delimit Scope ring_scope with ring.
+Import Order.POrderTheory Order.Theory Num.Theory GRing.Theory.
 
-Definition mul_RV (U : finType) (P : fdist U) (X Y : {RV (P) -> (R)}) (x : U) :=
-  X x * Y x.
+Notation R := real_realType.
+
+Definition mul_RV (U : finType) (P : {fdist U}) (X Y : {RV P -> R}) : {RV P -> R} := fun x => X x * Y x.
 Notation "X `* Y" := (mul_RV X Y) : proba_scope.
-Notation "X `* Y" := (fun x => X x * Y x) : proba_scope.
+Arguments mul_RV /.
 
 Section conj_intro_pattern.
 (* /[conj] by Cyril Cohen : *)
@@ -27,6 +29,36 @@ Lemma and_curry (A B C : Prop) : (A /\ B -> C) -> A -> B -> C.
 Proof. move=> + a b; exact. Qed.
 End conj_intro_pattern.
 Notation "[conj]" := (ltac:(apply and_curry)) (only parsing) : ssripat_scope.
+
+(* Section RV_ring. *)
+(* Variables (U : finType) (P : {fdist U}). *)
+(* Import topology. *)
+
+(* Lemma add_RV_addr (X Y : {RV P -> R}) : X `+ Y = (X + Y)%mcR. *)
+(* Proof. reflexivity. Qed. *)
+
+(* Lemma sub_RV_subr (X Y : {RV P -> R}) : X `- Y = (X - Y)%mcR. *)
+(* Proof. reflexivity. Qed. *)
+
+(* Lemma trans_min_RV_subr (X : {RV P -> R}) (y : R) : *)
+(*   X `-cst y = (X - cst y)%ring. *)
+(* Proof. reflexivity. Qed. *)
+(* Definition fdist_supp_choice : U. *)
+(* by move/set0Pn/xchoose:(fdist_supp_neq0 P). *)
+(* Defined. *)
+
+(* Canonical fdist_supp_pointedType := *)
+(*   @classical_sets.Pointed.pack U fdist_supp_choice _ _ idfun. *)
+
+(* Lemma mul_RV_mulr (X Y : {RV P -> R}) : X `* Y = (X * Y)%ring. *)
+(* Proof. reflexivity. Qed. *)
+
+(* Lemma sq_RV_sqrr (X : {RV P -> R}) : X `^2 = (X ^+ 2)%ring. *)
+(* Proof. by rewrite /sq_RV/comp_RV; apply boolp.funext => u /=; rewrite mulR1. Qed. *)
+
+(* Definition RV_ringE := *)
+(*   (add_RV_addr, sub_RV_subr, trans_min_RV_subr, mul_RV_mulr, sq_RV_sqrr). *)
+(* End RV_ring. *)
 
 
 Section sets_functions.
@@ -50,75 +82,27 @@ Proof.
   by move: H=> /[swap] /in_preim1 ->; rewrite eqxx.
 Qed.
 
-Lemma leq_sumR I r (P : pred I) (E1 E2 : I -> R) :
-    (forall i, P i -> E1 i <= E2 i) ->
-  \sum_(i <- r | P i) E1 i <= \sum_(i <- r | P i) E2 i.
-Proof. move=> leE12. elim/big_ind2: _ => // m1 m2 n1 n2. lra. Qed.
-
 Lemma Ind_subset (A : finType) (X Y : {set A}) :
   X \subset Y <-> forall a, Ind X a <= Ind Y a.
 Proof.
 rewrite /Ind; split => H.
-  by move=> a; case: ifPn; [move/(subsetP H) -> | case: (a \in Y)].
+  move=> a; case: ifPn.
+  - by move/(subsetP H) ->; apply/RleP.
+  - by case: (a \in Y) => _ //; apply/RleP.
 apply/subsetP => a aX.
-move: (H a); rewrite aX; case: (a \in Y) => //.
-by move/leRNgt/(_ Rlt_0_1).
+by move: (H a); rewrite aX; case: (a \in Y) => //; move/RleP; rewrite ler10.
 Qed.
 
 End sets_functions.
 
-Section ssrR_ext.
-
-End ssrR_ext.
-
-(*
-Section RV_ring.
-Variables (U : finType) (P : fdist U).
-Import topology.
-Import GRing.Theory.
-
-Lemma add_RV_addr (X Y : {RV P -> R}) : X `+ Y = (X + Y)%ring.
-Proof. reflexivity. Qed.
-
-Lemma sub_RV_subr (X Y : {RV P -> R}) : X `- Y = (X - Y)%ring.
-Proof. reflexivity. Qed.
-
-Lemma trans_min_RV_subr (X : {RV P -> R}) (y : R) :
-  X `-cst y = (X - cst y)%ring.
-Proof. reflexivity. Qed.
-Definition fdist_supp_choice : U.
-by move/set0Pn/xchoose:(fdist_supp_neq0 P).
-Defined.
-
-Canonical fdist_supp_pointedType :=
-  @classical_sets.Pointed.pack U fdist_supp_choice _ _ idfun.
-
-Lemma mul_RV_mulr (X Y : {RV P -> R}) : X `* Y = (X * Y)%ring.
-Proof. reflexivity. Qed.
-
-Lemma sq_RV_sqrr (X : {RV P -> R}) : X `^2 = (X ^+ 2)%ring.
-Proof. by rewrite /sq_RV/comp_RV; apply boolp.funext => u /=; rewrite mulR1. Qed.
-
-Definition RV_ringE :=
-  (add_RV_addr, sub_RV_subr, trans_min_RV_subr, mul_RV_mulr, sq_RV_sqrr).
-End RV_ring.
-*)
-
 Section probability.
-Variables (U : finType) (P : fdist U).
-Import GRing.Theory.
-
-Lemma Pr_lt1 (E : {set U}) : Pr P E < 1 <-> Pr P E != 1.
-Proof.
-have [Pr1|->] := (Pr_1 P E); first by rewrite ltR_eqF//.
-rewrite eq_refl/=; split => //; move/ltRR=>//.
-Qed.
+Variables (U : finType) (P : {fdist U}).
 
 Lemma sq_RVE (X : {RV P -> R}) : X `^2 = X `* X.
 Proof. by rewrite /sq_RV/comp_RV/=; apply: boolp.funext => x; rewrite mulR1. Qed.
 
 Lemma Ind_ge0 (X : {set U}) (x : U) : 0 <= Ind X x.
-Proof. by rewrite /Ind; case: ifPn. Qed.
+Proof. by rewrite /Ind; case: ifPn => // _; apply Rle_refl. Qed.
 
 Lemma Ind_setD (X Y : {set U}) :
   Y \subset X -> Ind (X :\: Y) = Ind X `- Ind Y :> {RV P -> R}.
@@ -136,7 +120,7 @@ Lemma cEx_ExInd (X : {RV P -> R}) F :
   `E_[X | F] = `E (X `* Ind (A:=U) F : {RV P -> R}) / Pr P F.
 Proof.
 rewrite /Pr /cEx (* need some lemmas to avoid unfolds *) -big_distrl /=.
-congr (_ / _).
+apply: congr2 => //.
 under eq_bigr => i _.
   rewrite big_distrr.
   have -> :
@@ -145,29 +129,23 @@ under eq_bigr => i _.
      (X i0 * @Ind U F i0 * P i0).
     apply congr_big => // i0.
     rewrite in_setI /Ind => /andP[] /in_preim1 -> ->.
-    by rewrite mulR1.
+    by rewrite !coqRE mulr1.
   have H1 :
     \sum_(i0 in finset (preim X (pred1 i)) :\: F) X i0 * Ind F i0 * P i0 = 0.
   (* This should be true because all elements of the sum are 0 *)
     rewrite big1 // => i1.
     rewrite in_setD => /andP [H2 H3].
-    by rewrite /Ind (negbTE H2) mulR0 mul0R.
+    by rewrite !coqRE /Ind (negbTE H2) mulr0 mul0r.
   have :
     \sum_(i0 in finset (preim X (pred1 i))) X i0 * Ind F i0 * P i0 =
     \sum_(i0 in finset (preim X (pred1 i)) :&: F) X i0 * Ind F i0 * P i0 +
     \sum_(i0 in finset (preim X (pred1 i)) :\: F) X i0 * Ind F i0 * P i0
     by apply big_setID.
-  rewrite H1 addR0 => <-.
+  rewrite !coqRE H1 addr0 => <-.
   under eq_bigl do rewrite in_preim1'.
   by over.
 by rewrite -partition_big_fin_img.
 Qed.
-
-Lemma sq_RV_ge0 (X : {RV P -> R}) x : 0 <= (X `^2) x.
-Proof. exact: pow2_ge_0. Qed.
-
-Lemma Ex_square_ge0 (X : {RV P -> R}) : 0 <= `E (X `^2).
-Proof. exact/Ex_ge0/sq_RV_ge0. Qed.
 
 Lemma Ex_square_expansion a b (X Y : {RV P -> R}):
   `E ((a `cst* X `+ b `cst* Y) `^2) =
@@ -178,7 +156,7 @@ suff : `E ((a `cst* X `+ b `cst* Y) `^2) =
        (b * b) `cst* (Y `^2) `+ (2 * a * b) `cst* (X `* Y)).
   by rewrite !E_add_RV !E_scalel_RV.
 apply eq_bigr => i H.
-unfold ambient_dist, "`cst*", "`+", "`^2", "`o", "^".
+unfold ambient_dist, "`cst*", "`+", "`^2", "`o", "^", "`*".
 nra.
 Qed.
 
@@ -190,8 +168,11 @@ split=> [XP|EX20].
   have [|->] := XP u; last by rewrite mulR0.
   by rewrite /sq_RV /comp_RV /= => ->; rewrite !mul0R.
 - have XP : forall x, x \in U -> (X `^2: {RV P -> R}) x * P x = 0.
-    apply: ((psumR_eq0P _).1 EX20).
-    by move=> u _; apply/mulR_ge0; [exact: sq_RV_ge0|exact: FDist.ge0].
+    move=> x Ux.
+    apply: (psumr_eq0P _ EX20) => // i Ui.
+    rewrite mulr_ge0//.
+    apply/RleP.
+    by apply: sq_RV_ge0.
   move=> x.
   have := XP x.
   rewrite inE => /(_ erefl) /mulR_eq0[|->]; last by right.
@@ -203,50 +184,56 @@ Lemma Cauchy_Schwarz_proba (X Y : {RV P -> R}):
 Proof.
 pose a := sqrt (`E (Y `^2)).
 pose b := sqrt (`E (X `^2)).
+have ? : 0 <= `E (X `^2) by apply: Ex_ge0; exact: sq_RV_ge0.
+have ? : 0 <= `E (Y `^2) by apply: Ex_ge0; exact: sq_RV_ge0.
 have H2ab : 2 * a * b * (b * a) = a * a * `E (X `^2) + b * b * `E (Y `^2).
-  rewrite -(Rsqr_sqrt (`E (X `^2))); last exact: Ex_square_ge0.
-  rewrite -(Rsqr_sqrt (`E (Y `^2))); last exact: Ex_square_ge0.
+  rewrite -(Rsqr_sqrt (`E (X `^2)))//.
+  rewrite -(Rsqr_sqrt (`E (Y `^2)))//.
   by rewrite -/a -/b /Rsqr; nra.
 have [a0|a0] := Req_dec a 0.
-  apply sqrt_eq_0 in a0; last exact: Ex_square_ge0.
+  apply sqrt_eq_0 in a0 => //.
   have HY : forall y, Y y = 0 \/ P y = 0 by apply/Ex_square_eq0/a0.
   have -> : `E (X `* Y: {RV P -> R}) = 0.
-    apply psumR_eq0P => u uU.
-      by case : (HY u) => -> ; rewrite mulR0 ?mul0R; apply leRR.
+    apply/eqP.
+    rewrite psumr_eq0.
+      apply/allP => u _; rewrite inE /=.
+      by case : (HY u) => -> ; rewrite mulR0 ?mul0R.
+    move => u _; rewrite /= .
     by case : (HY u) => -> ; rewrite mulR0 ?mul0R.
-  by rewrite Rsqr_0; apply/mulR_ge0; apply Ex_square_ge0.
-have [b0|b0] := Req_dec b 0.
-  apply sqrt_eq_0 in b0; last exact: Ex_square_ge0.
+  by rewrite Rsqr_0; apply/mulR_ge0.
+have [b0|b0] := Req_dec b 0. (* todo: replace with eqVneq.. *)
+  apply sqrt_eq_0 in b0 => //.
   have HX : forall x, X x = 0 \/ P x = 0 by apply /Ex_square_eq0/b0.
   have -> : `E (X `* Y: {RV P -> R}) = 0.
-    apply psumR_eq0P => u uU.
-      by case : (HX u) => -> ; rewrite ?mulR0 ?mul0R; apply leRR.
+    apply/eqP; rewrite psumr_eq0 /mul_RV; last first.
+      by move=> u _; case : (HX u) => -> ; rewrite ?mulR0 ?mul0R.
+    apply/allP => u _; rewrite inE/=.
     by case : (HX u) => -> ; rewrite ?mulR0 ?mul0R.
-  by rewrite Rsqr_0; apply/mulR_ge0; apply Ex_square_ge0.
+  by rewrite Rsqr_0; apply/mulR_ge0.
 have {}a0 : 0 < a (*removes a0 hypothesis and reuse it*)
   by apply/ltR_neqAle; split; [exact/nesym|exact/sqrt_pos].
 have {}b0 : 0 < b
   by apply/ltR_neqAle; split; [exact/nesym|exact/sqrt_pos].
-rewrite -(Rsqr_sqrt (_ * _)); last by apply/mulR_ge0; apply Ex_square_ge0.
-rewrite sqrt_mult; [|exact: Ex_square_ge0|exact: Ex_square_ge0].
-rewrite -/a -/b.
+rewrite -(Rsqr_sqrt (_ * _)); last by apply/mulR_ge0.
+rewrite sqrt_mult// -/a -/b.
 apply: neg_pos_Rsqr_le.
 - rewrite -(@leR_pmul2r (2 * a * b)); last first.
     by apply mulR_gt0 => //; apply mulR_gt0.
   rewrite -subR_ge0 mulNR subR_opp addRC mulRC H2ab.
-  by rewrite (mulRC (`E (X `* Y))) -Ex_square_expansion; exact: Ex_square_ge0.
+  rewrite (mulRC (`E (X `* Y))) -Ex_square_expansion.
+  by apply: Ex_ge0; exact: sq_RV_ge0.
 - apply/(@leR_pmul2l (2 * a * b)); first by do 2 apply: mulR_gt0 => //.
   apply/subR_ge0; rewrite H2ab -(Rmult_opp_opp b) -addR_opp -mulNR -mulRN.
-  by rewrite -Ex_square_expansion; exact: Ex_square_ge0.
+  by rewrite -Ex_square_expansion; apply: Ex_ge0; exact: sq_RV_ge0.
 Qed.
 
 Lemma I_square F : Ind F = ((Ind F) `^2 : {RV P -> R}).
 Proof.
-rewrite sq_RVE boolp.funeqE /Ind => x.
+rewrite sq_RVE boolp.funeqE /Ind /mul_RV => x.
 by case: ifPn; rewrite ?mulR0 ?mulR1.
 Qed.
 
-Lemma I_double F : Ind F = ((Ind F) `* (Ind F) : {RV P -> R}).
+Lemma I_double (F : {set U}) : Ind F = (Ind F) `* (Ind F) :> {RV P -> R}.
 Proof. by rewrite [LHS]I_square sq_RVE. Qed.
 
 (*
@@ -301,13 +288,13 @@ pose y := sqrt (Ex P (((X `-cst mu) `^2) `* Ind F) * Ex P (Ind F)) / Pr P F.
 apply leR_trans with (y := y).
   rewrite divRE leR_pmul2r // -sqrt_Rsqr_abs.
   apply sqrt_le_1_alt.
-  have -> : (X `-cst mu) `* Ind F = (X `-cst mu) `* Ind F `* Ind F
-    by rewrite {1}I_double boolp.funeqE=> u; rewrite mulRA.
+  have -> : (X `-cst mu) `* Ind F = (X `-cst mu) `* Ind F `* Ind F.
+    by rewrite {1}I_double boolp.funeqE=> u; rewrite /mul_RV mulRA.
   apply/(leR_trans (Cauchy_Schwarz_proba _ _))/leR_eqVlt; left.
   congr (_ * _); congr (`E _); last by rewrite -I_square.
   apply: boolp.funext => x/=.
   rewrite [in RHS]I_square.
-  by rewrite /sq_RV/=/comp_RV !mulR1 -mulRA [in LHS](mulRC (Ind F x)) !mulRA.
+  by rewrite /sq_RV/=/comp_RV/mul_RV !mulR1 -mulRA [in LHS](mulRC (Ind F x)) !mulRA.
 rewrite /y divRE -(sqrt_Rsqr (/ Pr P F)) // -sqrt_mult_alt; last first.
   move=> *; apply mulR_ge0; last by rewrite E_Ind.
   by apply: Ex_ge0 => u; apply: mulR_ge0; [apply pow2_ge_0 | apply Ind_ge0].
@@ -316,9 +303,9 @@ rewrite /var /cVar -/mu cEx_ExInd !E_Ind /Rsqr.
 rewrite mulRCA -!mulRA mulRV // mulR1 mulRC.
 rewrite [X in _ * X / _]mulRC mulRV // mulR1 divRE.
 apply leR_wpmul2r => //.
-apply leq_sumR=> i iU.
+apply leR_sumR=> i iU.
 rewrite -mulRA -[X in _ <= X]mulRA; apply leR_wpmul2l; first exact: sq_RV_ge0.
-by apply leR_pmul => //; [exact: Ind_ge0 | move/Ind_subset: FsubG | exact: leRR].
+by apply leR_pmul => //; [exact: Ind_ge0 | move/Ind_subset: FsubG; apply | apply/RleP].
 Qed.
 
 (*prove A1 and A3 for later use*)
@@ -370,8 +357,7 @@ Proof.
   unfold "`p_".
   rewrite -mulRDl.
   congr (_ * _).
-  unfold Ind.
-  rewrite in_setC.
+  rewrite /Ind/mul_RV in_setC.
   elim (i \in F); simpl; lra.
 Qed.
 
@@ -384,14 +370,16 @@ move=> PrPF_gt0 /[dup] /setIidPr GIFF FsubG /[dup] /(ltR_trans PrPF_gt0)
 have : 0 < Pr P (G :\: F) by rewrite Pr_diff subR_gt0 GIFF.
 move => /[dup] /Pr_gt0 PrPGF_neq0 PrPGF_gt0.
 rewrite !cEx_sub ?subsetDl // !divRE mulRCA.
-(*rewrite Ind_setD // !RV_ringE mulrDr mulrN E_sub_RV.
+rewrite Ind_setD//.
+rewrite !coqRE mulrAC divff// mul1r -!coqRE.
+rewrite (_ : _ `* (_ `- _) = (X `-cst `E_[X | G]) `* Ind G `- (X `-cst `E_[X | G]) `* Ind F); last first. (* TODO: use ring_RV *)
+  by rewrite /mul_RV/sub_RV; apply: boolp.funext => u /=; rewrite mulRBr.
+rewrite E_sub_RV.
 have -> : Ex P ((X `-cst `E_[X | G]) `* Ind G) = 0.
   apply normR0_eq0.
   by rewrite -(@eqR_mul2r (/ Pr P G)) // -divRE -cEx_sub // subRR normR0 mul0R.
-rewrite sub0R normRN.
-by rewrite [X in _ = _ * X]mulRAC mulRV // mul1R.
-Qed.*)
-Admitted.
+by rewrite sub0R normRN.
+Qed.
 
 (* NB: not used *)
 Lemma cEx_Inv (X: {RV P -> R}) F :
@@ -405,14 +393,15 @@ Qed.
 
 Lemma cvariance_nonneg (X : {RV P -> R}) F : 0 <= `V_[X | F].
 Proof.
-have [/ltRP H|] := boolP (0 <b Pr P F); last first.
-  rewrite -leRNlt' => /leRP.
+have [/RltP H|] := boolP (0 < Pr P F)%mcR; last first.
+  rewrite -leNgt => /RleP.
   move: (Pr_ge0 P F) => /[conj] /eqR_le H.
-  rewrite /cVar /cEx; apply big_ind; [exact: leRR|exact: addR_ge0|move=> i _].
-  by rewrite setIC Pr_domin_setI // mulR0 divRE mul0R; apply leRR.
+  rewrite /cVar /cEx; apply big_ind; [exact/RleP|exact: addR_ge0|move=> i _].
+  by rewrite setIC Pr_domin_setI // mulR0 divRE mul0R; exact/RleP.
 rewrite /cVar cEx_ExInd /Ex /ambient_dist divRE big_distrl /=.
-apply sumR_ge0 => u _; apply mulR_ge0; last exact/ltRW/invR_gt0.
-apply: mulR_ge0 => //; apply: mulR_ge0; first exact/sq_RV_ge0.
+apply/RleP; apply: sumr_ge0 => u _; rewrite !coqRE mulr_ge0//; last first.
+  by rewrite invr_ge0; apply/RleP.
+apply: mulr_ge0 => //; apply: mulr_ge0; first by apply/RleP; exact: sq_RV_ge0.
 by rewrite /Ind; by case: ifPn.
 Qed.
 
@@ -424,7 +413,7 @@ Lemma Ind_one F : Pr P F <> 0 -> `E_[Ind F : {RV P -> R} | F] = 1.
 Proof.
 move=> F0; rewrite cEx_ExInd.
 have -> : Ind F `* Ind F = Ind F.
-  by rewrite /Ind boolp.funeqE => u; case: ifPn; rewrite ?(mulR0,mulR1).
+  by move=> f; rewrite /mul_RV /Ind boolp.funeqE => u; case: ifPn; rewrite ?(mulR0,mulR1).
 by rewrite E_Ind divRE mulRV//; exact/eqP.
 Qed.
 
@@ -435,7 +424,7 @@ Proof.
   congr (_ * _).
   rewrite -E_add_RV.
   apply congr_big => // i HiU.
-  by rewrite mulRDl.
+  by rewrite /mul_RV mulRDl.
 Qed.
 
 Lemma cEx_sub_RV (X Y: {RV P -> R}) F: `E_[X `- Y | F] = `E_[X|F] - `E_[Y|F].
@@ -505,10 +494,8 @@ Qed.
 Lemma cEx_Pr_eq0 (X: {RV P -> R}) F:
   Pr P F = 0 -> `E_[X | F] = 0.
 Proof.
-  move => HPrF0.
-  apply psumR_seq_eq0P => [|a _| a _];
-  first apply undup_uniq;
-  by rewrite product_rule HPrF0 !mulR0 /Rdiv mul0R; try right.
+move => PrF0.
+by rewrite cEx_ExInd PrF0 !coqRE invr0 mulr0.
 Qed.
 
 Lemma cVarE (X : {RV (P) -> (R)}) F:
@@ -548,7 +535,7 @@ Notation "`V_[ X | F ]" := (cVar X F) : proba_scope.
 Arguments Ind_one {U P}.
 
 Section resilience.
-Variables (U : finType) (P : fdist U).
+Variables (U : finType) (P : {fdist U}).
 
 Lemma cresilience (delta : R) (X : {RV P -> R}) (F G : {set U}) :
   0 < delta -> delta <= Pr P F / Pr P G -> Pr P F < Pr P G -> F \subset G ->
@@ -621,7 +608,7 @@ Qed.
 End resilience.
 
 Section robustmean.
-Variables (U : finType) (P : fdist U).
+Variables (U : finType) (P : {fdist U}).
 
 Theorem robust_mean (good drop: {set U}) (X : {RV P -> R}) (eps : R):
   let bad := ~: good in
@@ -693,11 +680,11 @@ have Exbad_bound : 0 < Pr P (bad :\: drop) ->
       \sum_(i in U) (X i - mu) * @Ind U (bad :\: drop) i * P i); last first.
     by apply: eq_bigr => u _; rewrite -mulRA mulNR addR_opp -mulRBl mulRA.
   rewrite normRM (geR0_norm (/ _)); last exact/ltRW/invR_gt0.
-  apply/leR_pdivr_mulr => //; apply: (leR_trans (leR_sumR_Rabs _)).
+  apply/leR_pdivr_mulr => //; apply: (leR_trans (leR_sumR_Rabs _ _)).
   rewrite (bigID [pred i | i \in bad :\: drop]) /=.
   rewrite [X in _ + X]big1 ?addR0; last first.
     by move=> u /negbTE abaddrop; rewrite /Ind abaddrop mulR0 mul0R normR0.
-  rewrite /Pr big_distrr /=; apply leq_sumR => i ibaddrop.
+  rewrite /Pr big_distrr /=. apply leR_sumR => i ibaddrop.
   rewrite normRM (geR0_norm (P i))//; apply: leR_wpmul2r => //.
   rewrite /Ind ibaddrop mulR1.
   move: ibaddrop; rewrite inE => /andP[idrop ibad].
